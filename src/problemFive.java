c4a02673d1caec3b2c8abbb22d27be40a806735f
Christopher Garcia <Christopherg1818@gmail.com>
2025-01-25 21:51:52 -0600

    ThirdQuestion

--- src/problemFive.java
+++ src/problemFive.java
@@ -1,15 +1,18 @@
+//01/21/2024
+//Christopher Garcia
+//Introduction to Algorithms
 public class problemFive {
     class Solution {
         public int removeElement(int[] nums, int val) {
-            int i = 0;
-            int j = 0;
-            for (j = 0; j < nums.length; j++) {
-                if (nums[j] != val) {
-                    nums[i] = nums[j];
-                    i++;
+            int i = 0; //Constant 0(1)
+            int j = 0; //Constant 0(1)
+            for (j = 0; j < nums.length; j++) { //0(N)
+                if (nums[j] != val) { //Constant 0(1)
+                    nums[i] = nums[j];//Constant 0(1)
+                    i++; //Constant 0(1)
                 }
             }
-            return i;
+            return i;//Constant 0(1)
         }
     }
 }
