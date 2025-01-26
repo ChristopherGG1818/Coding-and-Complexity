4fdc93c03d4ceed9f7555bf68d2b98819f2c1e6e
Christopher Garcia <Christopherg1818@gmail.com>
2025-01-25 21:52:03 -0600

    ThirdQuestion

--- src/problemThree.java
+++ src/problemThree.java
@@ -1,19 +1,23 @@
+//01/21/2024
+//Christopher Garcia
+//Introduction to Algorithms
 public class problemThree {
     public static void main(String[] args) {
-        int terms = 10;
-        System.out.println( terms );
-        NotFibonacci(terms);
+        int terms = 10; //Constant 0(1)
+        System.out.println( terms ); //Constant 0(1)
+        NotFibonacci(terms); //(n)
     }
     public static void NotFibonacci(int terms) {
-        long A = 0;
-        long B = 1;
-        System.out.print(A + ", " + B);
-        for (int i = 2; i < terms; i++) {
-            long current = (3 * B) + (2 * A);
-            System.out.print(", " + current);
-            A = B;
-            B = current;
+        long A = 0; //Constant 0(1)
+        long B = 1; //Constant 0(1)
+        System.out.print(A + ", " + B);//Constant 0(1)
+
+        for (int i = 2; i < terms; i++) { //0(N) looop runnnig N times
+            long current = (3 * B) + (2 * A);//Constant 0(1)
+            System.out.print(", " + current); //Constant 0(1)
+            A = B; //Constant 0(1)
+            B = current; //Constant 0(1)
         }
-        System.out.println();
+        System.out.println(); //Constant 0(1)
     }
 }
