0f4c85b2937b9d246e5e5bd345e63283f95dba1d
Christopher Garcia <Christopherg1818@gmail.com>
2025-01-25 21:51:18 -0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,31 @@
 //Introduction to Algorithms
 public class Main {
     public static int problemOne(String text1, String text2) {
-        int A = text1.length(); //Constant
-        int B = text2.length(); //Constant
+        int A = text1.length(); //Constant 0(1)
+        int B = text2.length(); //Constant 0(1)
 
-        int[] count = new int[B + 1];
-        for (int i = 1; i <= A; i++) {
-            int prev = 0; //Constant
-            for (int j = 1; j <= B; j++) {
-                int temp = count[j];
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    count[j] = prev + 1;
+        int[] count = new int[B + 1]; //0(1)
+        for (int i = 1; i <= A; i++) { //(A) loop
+            int prev = 0; //Constant 0(1)
+            for (int j = 1; j <= B; j++) { //(B) nested b loop
+                int temp = count[j]; //Constant 0(1)
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) { //Constant 0(1)
+                    count[j] = prev + 1; //Constant 0(1)
                 } else {
-                    count[j] = Math.max(count[j], count[j - 1]);
+                    count[j] = Math.max(count[j], count[j - 1]); //Constant 0(1)
                 }
-                prev = temp;
+                prev = temp; //Constant 0(1)
             }
         }
-        return count[B];
+        return count[B]; //Constant 0(1)
     }
     public static void main(String[] args) {
         //String text1 = ("abc");
         //String text2 = ("abc");
         //String text1 = "almanacs";
-        //String text2 = ("albatross");
+        String text2 = ("albatross");
         String text1 = ("almanac");
-        String text2 = ("ferris");
+        //String text2 = ("ferris");
         System.out.println(problemOne(text1, text2));
     }
 }
